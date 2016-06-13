4668e40e8e885ced1367f898fc3259aa3ae97ce9
mfournier91 <marc@Marcs-MacBook-Pro.local>
2016-06-12 21:44:06 -0400

    Added game logic to main.js, personalized index.html, added css from units 1-5 along with some personalized styling

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,1 +1,4 @@
-console.log("JS file is connected to HTML! Woo!")
+//console.log("JS file is connected to HTML! Woo!");
+
+// Create str values for each card
+var cardOne = "queen", cardTwo = "king", cardThree = "queen", cardFour = "king";
