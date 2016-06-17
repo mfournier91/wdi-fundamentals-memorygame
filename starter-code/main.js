a92373a1a55dd3181a980c09efb0aa93b1051e76
mfournier91 <marc@Marcs-MacBook-Pro.local>
2016-06-17 17:56:11 -0400

    Added if statement logic. Removed html card divs and used JS to create card divs.

--- starter-code/main.js
+++ starter-code/main.js
@@ -2,3 +2,26 @@
 
 // Create str values for each card
 var cardOne = "queen", cardTwo = "king", cardThree = "queen", cardFour = "king";
+
+/*if (cardTwo === cardFour) {
+	alert("You found a match!");
+}
+if (cardOne === cardThree) {
+	alert("You found a match!");
+}
+if (cardOne === cardTwo) {
+	alert("Sorry, try again.");
+}*/
+
+var createBoard = function(){
+	var gameBoard = document.getElementById('game-board');
+	console.log(gameBoard);
+	for (var i = 0; i < 4; i++) {
+		var newCard = document.createElement('div');
+		//newCard.className = "card";
+		gameBoard.appendChild(newCard);
+		createdCard = document.getElementsByTagName('div')[i + 1];
+    createdCard.className = "card";
+	}
+}
+createBoard();
