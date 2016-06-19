8c904d25f99732bcbf6bb5d789f7ca4967251909
mfournier91 <marc@Marcs-MacBook-Pro.local>
2016-06-19 00:41:46 -0400

    Added used and unused background images for cards. Implemented arrays for cards. New function that creates the board and cards provides data for the cards and makes the cards clickable. New function that is run when card is clicked that checks if two cards have been clicked. Finally a new function to check if the two cards are a match

--- starter-code/main.js
+++ starter-code/main.js
@@ -1,27 +1,59 @@
-//console.log("JS file is connected to HTML! Woo!");
 
-// Create str values for each card
-var cardOne = "queen", cardTwo = "king", cardThree = "queen", cardFour = "king";
+//Third Function called in isTwoCards
+var isMatch = function(twoCards){
 
-/*if (cardTwo === cardFour) {
-	alert("You found a match!");
-}
-if (cardOne === cardThree) {
-	alert("You found a match!");
-}
-if (cardOne === cardTwo) {
-	alert("Sorry, try again.");
-}*/
+	if (twoCards[0] === twoCards[1]) {
+		alert("You found a match!");
+		
+	}
+	else {
+		alert("Sorry, try again.");
 
+		//Tried to select only cards that were mismatches in the following commented out code; 
+		//However, this only works the first time. Thereafter, the first two divs
+		// on the page that have background images are selected.
+
+		/*var mismatched =  [document.querySelectorAll('.card img')[0], document.querySelectorAll('.card img')[1]];
+		var mismatchedP = [mismatched[0].parentNode, mismatched[1].parentNode];
+		mismatchedP[0].innerHTML = '';
+		mismatchedP[1].innerHTML = '';*/
+		
+		
+	}
+	var allCards = document.getElementsByClassName('card');
+	for (i = 0; i < allCards.length; i++){
+	allCards[i].innerHTML = ''; //Quirk: sometimes, the second card never appears. InnerHTML is assigned before alert yet this happens.
+	}
+};
+
+//Second Function called by clicking on cards created in first function
+function isTwoCards() {
+	cardsInPlay.push(this.getAttribute('data-card')); //add clicked card's data to cardsInPlay
+	this.innerHTML = '<img src="MadisonTeal/' + this.getAttribute('data-card') + '.png" />' //add background image of given appropriate card data;
+	
+	if (cardsInPlay.length === 2) {
+		isMatch(cardsInPlay);
+		cardsInPlay = [];
+	}
+};
+
+//Start Parsing here
+var cards = ['queen', 'queen', 'king', 'king'];
+var cardsInPlay = [];
+
+//First function
 var createBoard = function(){
 	var gameBoard = document.getElementById('game-board');
-	console.log(gameBoard);
-	for (var i = 0; i < 4; i++) {
+	
+	//create cards on the board
+	for (var i = 0; i < cards.length; i++) {
 		var newCard = document.createElement('div');
-		//newCard.className = "card";
 		gameBoard.appendChild(newCard);
-		createdCard = document.getElementsByTagName('div')[i + 1];
-    createdCard.className = "card";
+		var cardElement = document.getElementsByTagName('div')[i + 1];
+    cardElement.className = "card";
+    cardElement.setAttribute('data-card', cards[i]);
+    cardElement.addEventListener('click', isTwoCards);
+    //console.log(cardElement);
 	}
-}
+};
 createBoard();
